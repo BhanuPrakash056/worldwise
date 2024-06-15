791de3dd3cbbfe354c766b89e63e51f835c5cf82
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-06-15 21:13:03 +0530

    feat: add a router for the componenta

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Product";
+
+function HomePage() {
+  return (
+    <div>home</div>
+  );
+}
+
+export default HomePage;
