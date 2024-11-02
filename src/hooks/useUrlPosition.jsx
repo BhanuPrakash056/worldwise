a56b6b096c3a481f93f96ada5113f9a55062e916
bhanu prakash <r19cs056@cit.reva.edu.in>
2024-11-02 23:37:46 +0530

    docs: add comments for useUrlPosition

--- src/hooks/useUrlPosition.jsx
+++ src/hooks/useUrlPosition.jsx
@@ -1,5 +1,18 @@
 import { useSearchParams } from "react-router-dom";
 
+/**
+ * Custom hook to retrieve latitude and longitude from the URL search parameters.
+ *
+ * This hook uses the `useSearchParams` hook from `react-router-dom` to extract
+ * the values of `lat` and `lng` from the current URL. It returns an array
+ * containing the latitude and longitude as strings. If the parameters are not
+ * present in the URL, the returned values will be `null`.
+ *
+ * Usage:
+ * const [latitude, longitude] = useUrlPosition();
+ *
+ * @returns {[string|null, string|null]} An array containing the latitude and longitude.
+ */
 export function useUrlPosition() {
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
